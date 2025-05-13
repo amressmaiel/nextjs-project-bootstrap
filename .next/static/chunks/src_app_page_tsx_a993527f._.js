013f727725554547a3347ed949d159199d9f58db
amressmaiel <AMRESSMAIEL@GMAIL.COM>
2025-05-13 12:58:14 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747130294458

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9d1b38a3._.js",
+  "static/chunks/node_modules_xlsx_xlsx_mjs_ad755052._.js",
+  "static/chunks/node_modules_jspdf_dist_jspdf_es_min_c277e70f.js",
+  "static/chunks/node_modules_0bffb4a1._.js",
+  "static/chunks/src_app_page_tsx_b025fed5._.js"
+],
+    source: "dynamic"
+});
